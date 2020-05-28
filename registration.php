653ad4ebe89ca459acaf465783184000207def59
solwininfotech <info@solwininfotech.com>
2020-05-28 19:59:33 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Quick View Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_QuickView
+ * @copyright  Copyright © 2006-2017 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_QuickView',
+    __DIR__
+);
